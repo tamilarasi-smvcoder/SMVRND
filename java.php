47312f7ddb0941b828c37bff9bbe776a0a5770f7
smc21devaloper <smvcool21@gmail.com>
2023-12-27 21:11:08 +0530

    Correction on Index File

--- java.php
+++ java.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="button.css"/>
-
+    <link rel="stylesheet" href="css/button.css"/>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai-sublime.min.css">
     <!-- Choose a different style from https://github.com/highlightjs/highlight.js/tree/main/src/styles -->
@@ -86,7 +85,7 @@
 
 </style>
 <body class="prevent-select">
-<h1 class="Purples">JAVA TUTORIAL</h1>
+<h1>JAVA TUTORIAL</h1>
     <div class="container">
         
         <div class="row">

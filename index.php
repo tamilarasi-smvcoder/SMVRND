0614de474600d3b5c3b26b08d3eacae4a4c8ffb4
tamilarasi-smvcoder <tamilarasig21@gmail.com>
2023-12-27 21:34:02 +0530

    Alignment

--- index.php
+++ index.php
@@ -6,14 +6,12 @@
     <title>SMV Research and Development</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css"/>
-
-</head>
+  </head>
   <body class="vh-100 overflow-hidden">
     <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#"></a>
           <img src="Images/logo.png" alt="" width="100" height="100">
-        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
